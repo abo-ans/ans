e6cf9b6ec842df23805e1d4c87d510031615d18d
abo-ans <76831531+abo-ans@users.noreply.github.com>
2022-12-01 09:45:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php
-
-
-list($width, $height, $type, $attr) = getimagesize("img/1.jpg");
-echo  $width .'<br>fff';
-echo $height.'<br>';
-echo $type.'<br>';
-echo $attr.'<br>';
-///echo "<img src=\"img/1.jpg\" $attr alt=\"getimagesize() example\" />";
-?>
+ldkjlfjsdf
