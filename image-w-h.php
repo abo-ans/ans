60691bc436c011f912dc584c34677037f68023a6
abo-ans <76831531+abo-ans@users.noreply.github.com>
2022-12-01 09:32:20 +0300

    Add files via upload

---
+++ image-w-h.php
@@ -0,0 +1,8 @@
+<?php
+list($width, $height, $type, $attr) = getimagesize("img/1.jpg");
+echo  $width .'<br>';
+echo $height.'<br>';
+echo $type.'<br>';
+echo $attr.'<br>';
+///echo "<img src=\"img/1.jpg\" $attr alt=\"getimagesize() example\" />";
+?>
